ee32cd65d9bef5382efd55882e307be35c8f746a
Rob <rob.baars@hotmail.nl>
2023-01-10 19:47:14 +0100

    add install script for my tools

---
+++ .monstertov/install.sh
@@ -0,0 +1,23 @@
+# install my default systadmin/dev packages
+
+# Requirements: apt
+
+# toadd: wemux?
+
+
+# install if apt
+sudo apt update
+
+sudo apt install curl neofetch nmap snapd tmux wireshark python3 python3-pip
+
+# install snapd core
+sudo snap install core
+
+# googlr tool https://github.com/Astranno/googlr
+curl -fsSL https://raw.githubusercontent.com/Astranno/googlr/master/Install%20Scripts/install.sh | sudo sh
+
+# histstat tool https://github.com/vesche/histstat
+sudo pip install histstat
+
+# bashtop tool
+sudo snap install bashtop
