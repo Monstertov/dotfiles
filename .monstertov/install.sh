0563879d62ffee7c166495532b8782e5fd142821
Monstertov <Monstertov@users.noreply.github.com>
2023-11-02 21:23:07 +0100

    Update install.sh

--- .monstertov/install.sh
+++ .monstertov/install.sh
@@ -8,7 +8,7 @@
 # install if apt
 sudo apt update
 
-sudo apt install curl neofetch nmap snapd tmux wireshark python3 python3-pip lolcat htop proxychains4
+sudo apt install curl neofetch nmap snapd tmux wireshark python3 python3-pip lolcat htop proxychains4 python3-full
 
 # install snapd core
 sudo snap install core
