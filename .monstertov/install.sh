ed62fae37b46e5494cab7fb0dd24d5406c25b0b2
monstertov <rob@monstertov.nl>
2024-06-19 19:44:23 +0200

    added tailscale

--- .monstertov/install.sh
+++ .monstertov/install.sh
@@ -2,11 +2,22 @@
 # Requirements: apt
 
 
+# add sources
+# Define the Debian version
+DEBIAN_VERSION=$(lsb_release -cs)
+
+# Add Tailscale's GPG key
+curl -fsSL https://pkgs.tailscale.com/stable/debian/${DEBIAN_VERSION}.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+# Add Tailscale's repository
+curl -fsSL https://pkgs.tailscale.com/stable/debian/${DEBIAN_VERSION}.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+
+
 # install if apt
 sudo apt update
 
+
 # package list
-sudo apt install curl neofetch nmap snapd tmux wireshark python3 python3-pip lolcat htop proxychains4 python3-full git tree davfs2 cifs-utils ufw tightvncserver speedtest-cli -y
+sudo apt install curl neofetch zsh nmap snapd tmux wireshark python3 python3-pip lolcat htop proxychains4 python3-full git tree davfs2 cifs-utils ufw tightvncserver speedtest-cli tailscale -y
 
 # install snapd core
 sudo snap install core
@@ -20,7 +31,6 @@ sudo snap install core
 # vscode requirements
 sudo apt install software-properties-common apt-transport-https wget -y
 
-
 # setup ufw
 sudo ufw allow 22
 
@@ -30,3 +40,4 @@ sudo apt-get update
 sudo apt-get install anydesk -y
 
 sudo apt autoremove -y
+
