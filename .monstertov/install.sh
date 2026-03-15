203e43ebccff92ac47fd365bed6c900eba307172
Monstertov <rob@monstertov.nl>
2026-03-15 18:03:49 +0100

    cleanup

--- .monstertov/install.sh
+++ .monstertov/install.sh
@@ -17,10 +17,10 @@ sudo apt update
 
 
 # package list
-sudo apt install curl neofetch zsh nmap snapd tmux wireshark python3 python3-pip lolcat htop proxychains4 python3-full git tree davfs2 cifs-utils ufw tightvncserver speedtest-cli tailscale -y
+sudo apt install curl neofetch nmap snapd tmux wireshark python3 python3-pip lolcat htop proxychains4 python3-full git tree davfs2 cifs-utils gawk ufw tightvncserver ncdu speedtest-cli tailscale -y
 
 # install snapd core
-sudo snap install core
+#sudo snap install core
 
 # googlr tool https://github.com/Astranno/googlr
 #curl -fsSL https://raw.githubusercontent.com/Astranno/googlr/master/Install%20Scripts/install.sh | sudo sh
@@ -33,11 +33,6 @@ sudo apt install software-properties-common apt-transport-https wget -y
 
 # setup ufw
 sudo ufw allow 22
-
-# install anydesk
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add - 
-sudo apt-get update
-sudo apt-get install anydesk -y
 
 sudo apt autoremove -y
 
